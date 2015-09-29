a2be0dcb02673e40ae2767ab0cd1e59ddacd85c0
lizasavanovich <liza.savanovich@mail.ru>
2015-09-29 16:24:42 +0300

    ArrayApp

---
+++ SortArrays/LogicUnitTest/UnitTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SortArrays;
+
+
+namespace LogicUnitTest
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestSortSumStr()
+        {
+            int[][] A = new int[3][];
+            A[0] = new int[] { 1, 3, 5 };
+            A[1] = new int[] { 9, 10 };
+            A[2] = new int[] { 0, 2, 4, 6 };
+
+            int[][] B = new int[3][];
+            B[0] = new int[] { 1, 3, 5 };
+            B[1] = new int[] { 0, 2, 4, 6 };
+            B[2] = new int[] { 9, 10 };
+
+            Logic.SortSum(A);
+
+            int n = 0;
+
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < A[i].Length; j++)
+                {
+                    if (A[i][j] != B[i][j])
+                    {
+                        n = 1;
+                    }
+                }
+            }
+
+            Assert.AreEqual(0, n);
+        }
+
+       [TestMethod]
+        public void TestSortMaxElem()
+        {
+            int[][] A = new int[3][];
+            A[0] = new int[] { 1, 3, 5 };
+            A[1] = new int[] { 9, 10 };
+            A[2] = new int[] { 0, 2, 4, 6 };
+
+            int[][] B = new int[3][];
+            B[0] = new int[] { 1, 3, 5 };
+            B[1] = new int[] { 0, 2, 4, 6 };
+            B[2] = new int[] { 9, 10 };
+
+            Logic.SortMaxElem(A);
+
+            int n = 0;
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < A[i].Length; j++)
+                {
+                    if (A[i][j] != B[i][j])
+                    {
+                        n = 1;
+                    }
+                }
+            }
+
+            Assert.AreEqual(0, n);
+        }
+    
+     [TestMethod]
+        public void TestSortMinElem()
+        {
+            int[][] A = new int[3][];
+            A[0] = new int[] { 9, 10 };
+            A[1] = new int[] { 1, 3, 5 };
+            A[2] = new int[] { 0, 2, 4, 6 };
+
+            int[][] B = new int[3][];
+            B[0] = new int[] { 0, 2, 4, 6 };
+            B[1] = new int[] { 1, 3, 5 };
+            B[2] = new int[] { 9, 10 };
+
+            Logic.SortMinElem(A);
+
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < A[i].Length; j++)
+                {
+                    Console.WriteLine(A[i][j]);
+                }
+            }
+
+            int n = 0;
+
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < A[i].Length; j++)
+                {
+                    if (A[i][j] != B[i][j])
+                    {
+                        n = 1;
+                    }
+                }
+            }
+
+            Assert.AreEqual(0, n);
+            }
+}
+
+}
---
+++ SortArrays/SortArrays/Logic.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortArrays
+{
+    public static class Logic
+    {
+        public static void SortSum(int[][] a) {
+            int[] ArraySum = new int[a.GetLength(0)];         
+            for (int i = 0; i < a.GetLength(0)-1; i++)
+            {
+                ArraySum[i] = Sum(a[i]);
+            }
+           
+            for (int i = 0; i < a.GetLength(0) - 1; i++)
+            {
+                for (int j = 0; j < a.GetLength(0) - i - 1; j++)
+                {
+                    if (ArraySum[j] > ArraySum[j + 1])
+                        Swap(ref a[j], ref a[j + 1]);
+
+                }
+            }
+        }
+        private static int Sum(int[] b)
+        {
+            int s = 0;
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                s = s + b[i];
+            }
+            return s;
+        }
+
+        public static void SortMaxElem(int[][] a)
+        {
+            int[] ArrayMaxElem = new int[a.GetLength(0)];
+            for (int i = 0; i < a.GetLength(0) - 1; i++)
+            {
+                ArrayMaxElem[i] = MaxElem(a[i]);
+            }
+            for (int i = 0; i < a.GetLength(0) - 1; i++)
+            {
+                for (int j = 0; j < a.GetLength(0) - i - 1; j++)
+                {
+                    if (ArrayMaxElem[j] > ArrayMaxElem[j + 1])
+                        Swap(ref a[j], ref a[j + 1]);
+
+                }
+            }
+        }
+        private static int MaxElem(int[] b)
+        {
+            int s = b[0];
+            for (int i = 0; i < b.Length; i++)
+            {
+                if (s < b[i]) s = b[i];
+            }
+            return s;
+        }
+
+        public static void SortMinElem(int[][] a)
+        {
+            int[] ArrayMinElem = new int[a.GetLength(0)];
+            for (int i = 0; i < a.GetLength(0) - 1; i++)
+            {
+                ArrayMinElem[i] = MinElem(a[i]);
+            }
+
+            for (int i = 0; i < a.GetLength(0) - 1; i++)
+            {
+                for (int j = 0; j < a.GetLength(0) - i - 1; j++)
+                {
+                    if (ArrayMinElem[j] > ArrayMinElem[j + 1])
+                        Swap(ref a[j], ref a[j + 1]);
+
+                }
+            }
+                     
+        }
+
+        private static int MinElem(int[] b)
+        {
+            int s = b[0];
+            for (int i = 0; i < b.Length; i++)
+            {
+                if (s > b[i]) s = b[i];
+            }
+            return s;
+        }
+            
+        private static void Swap(ref int[] k, ref int[] l)
+        {
+            int[] b = k;
+            k = l;
+            l = b;
+        }
+    }
+}
